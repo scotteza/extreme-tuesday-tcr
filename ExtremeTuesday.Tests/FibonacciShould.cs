cc2ce22320db0dbdd053767ea34fdd9ee47783c5
Scott Edwards <scotteza@gmail.com>
2019-01-08 19:28:40 +0000

    Add zero case

---
+++ ExtremeTuesday.Tests/FibonacciShould.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace ExtremeTuesday.Tests
+{
+    [TestFixture]
+    internal class FibonacciShould
+    {
+        [Test]
+        public void Return_Zero_On_Zero_Input()
+        {
+            var fibonacci = new Fibonacci();
+
+            var result = fibonacci.Of(0);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
---
+++ ExtremeTuesday/Fibonacci.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtremeTuesday
+{
+    public class Fibonacci
+    {
+        public int Of(int input)
+        {
+            return 0;
+        }
+    }
+}
